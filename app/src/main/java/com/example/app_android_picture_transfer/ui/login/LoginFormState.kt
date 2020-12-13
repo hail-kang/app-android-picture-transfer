0fff2d111c75b2a0fb27fc982ab7105db3125b70
hail-kang <73281731+hail-kang@users.noreply.github.com>
2020-12-13 22:31:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/app_android_picture_transfer/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.example.app_android_picture_transfer.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
